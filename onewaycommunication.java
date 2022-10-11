ff29609457893e88ad156da0fb2134b9b545a6db
Prathamesh2642 <76043914+Prathamesh2642@users.noreply.github.com>
2022-10-11 15:04:42 +0530

    Create onewaycommunication.java

---
+++ onewaycommunication.java
@@ -0,0 +1,39 @@
+// Server Side
+import java.io.*;
+import java.net.*;
+
+public class ABC {
+    public static void main(String[] args) {
+        try {
+            ServerSocket ss = new ServerSocket(6666);
+            Socket s=ss.accept();
+            DataInputStream di=new DataInputStream(s.getInputStream());
+            String str=di.readUTF();
+            System.out.println("Message from client "+str);
+            ss.close();
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+    }
+}
+
+// Client side
+import java.io.*;
+import java.net.*;
+
+public class PQR {
+    public static void main(String[] args){
+        try {
+            Socket s = new Socket("localhost", 6666);
+            DataOutputStream ds = new DataOutputStream(s.getOutputStream());
+            ds.writeUTF("Hi!!! Hello");
+            ds.flush();
+            ds.close();
+            s.close();
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+    }
+}
